855b853f29582d76e35ce99e69e3e41fb1389ff2
Robert L. Read <read.robert@gmail.com>
2017-06-04 12:50:17 -0500

    some progress on Triangle, and other work

--- test/test_fmin.js
+++ test/test_fmin.js
@@ -6,28 +6,12 @@ var fmin = require('../javascripts/fmin.js');
 var algols = require('algorithms').DataStructures;
 var THREE = require('../javascripts/three.js');
 
-console.log(fmin);
+// console.log(fmin);
 
 var SMALL = 1e-5;
 
-var x = 1.6084564160555601, y = -1.5980748860165477;
-function banana(X, fxprime) {
-    fxprime = fxprime || [0, 0];
-    var x = X[0], y = X[1];
-    fxprime[0] = 400 * x * x * x - 400 * y * x + 2 * x - 2;
-    fxprime[1] = 200 * y - 200 * x * x;
-    return (1 - x) * (1 - x) + 100 * (y - x * x) * (y - x * x);
-}
-
-
 describe('fmin', function() {
-    describe('banana function', function() {
-	var params = {'learnRate' : 0.0003, 'maxIterations' : 50000};	
-        var solution = fmin.conjugateGradient(banana, [x, y], params);
-	console.log(solution);
-    });
-
-    describe('zero function', function() {
+/*    describe('zero function', function() {
 	var params = {'maxIterations' : 500, 'history' : []};
 
 	var fxprime = [12,12];
@@ -42,5 +26,127 @@ describe('fmin', function() {
 	console.log(solution);
     });
 
+*/
+    describe('simple trinagle case', function() {
+	var params = {'maxIterations' : 500, 'history' : []};
+
+	/* An attempt to set up the simplest triangle we can 
+for the purpose of testing fmin and our ability to define the gradient 
+satisfactorily. In this model "X" will be a six vector (3 x 2), representing
+3 x,y variables. G[0] = <0,0>, G[1] = <0,1>, G[2] = < 0.5, sqrt(3)/2>.
+*/
+	var fxprime = [0,0,0,0,0,0];
+
+	var H = [0,0,0,1,0.5,Math.sqrt(3)/2];
+	var G = [[0,1],[1,2],[0,2]];
+	var f = function(X,fxprime) {
+	    // We define f to be the sum or the reward function and penalty function
+	    // Although we could operate on fxprime, I prefer pure functions,
+	    // so I will define r and p to be pure functions that return the pair [f(X),f'(X)].
+	    // Note that f(X) is a scalar, and f'(X) is a vector in the same shape as X.
+	    var rv = r(X,H);
+	    var pv = p(X,G);
+	    // against my preferred style, the fmin system requires us to modify the fxprime parameter
+	    for(var i = 0; i < rv[1].length; i++) {
+		fxprime[i] = rv[1][i] + pv[1][i];		
+	    }
+	    console.log("fxprime",fxprime);
+	    console.log(rv);
+	    console.log(pv);
+	    return rv[0]+pv[0];
+	}
+	
+	var r = function(X,H) {
+	    var v = 0;
+	    H.forEach(function(x,i) {
+		v += (X[i] - H[i])*(X[i] - H[i]);
+	    });
+	    var d = [0,0,0,0,0,0];
+	    d.forEach(function(e,i) {
+		d[i] = 2*(X[i] - H[i]);
+	    });
+	    console.log("d",d);
+	    return [v,d];
+	}
+	
+	var p = function(X,G) {
+	    var v = 0;
+	    var d = [0,0,0,0,0,0];
+	    return [v,d];	    
+	}
+
+        var solution = fmin.conjugateGradient(f, fxprime, params);
+	console.log(solution);
+    });
+
+
+    describe('parametric trinagle case', function() {
+	var params = {'maxIterations' : 500, 'history' : []};
+
+	/* An attempt to set up the simplest triangle we can 
+for the purpose of testing fmin and our ability to define the gradient 
+satisfactorily. In this model "X" will be a six vector (3 x 2), representing
+3 x,y variables. G[0] = <0,0>, G[1] = <0,1>, G[2] = < 0.5, sqrt(3)/2>.
+*/
+	var fxprime = [0,0,0,0,0,0];
+
+	var H = [0,0,0,1,0.5,Math.sqrt(3)/2]; // Goal positions.
+	var E = [[0,1],[1,2],[0,2]]; // Edges
+	var N = [0,1,2]; // Node 
+	var Y = [0.5,0.5,0.5]; // minimum edge lengths
+	var Z = [1.0,1.0,1.0]; // maximum edge lengths
+	var f = function(X,fxprime) {
+	    // We define f to be the sum or the reward function and penalty function
+	    // Although we could operate on fxprime, I prefer pure functions,
+	    // so I will define r and p to be pure functions that return the pair [f(X),f'(X)].
+	    // Note that f(X) is a scalar, and f'(X) is a vector in the same shape as X.
+	    var rv = r(X,H);
+	    var pv = p(X,E);
+	    // against my preferred style, the fmin system requires us to modify the fxprime parameter
+	    for(var i = 0; i < rv[1].length; i++) {
+		fxprime[i] = rv[1][i] + pv[1][i];		
+	    }
+	    console.log("fxprime",fxprime);
+	    console.log(rv);
+	    console.log(pv);
+	    return rv[0]+pv[0];
+	}
+	
+	var r = function(X,H) {
+	    var v = 0;
+	    H.forEach(function(x,i) {
+		v += (X[i] - H[i])*(X[i] - H[i]);
+	    });
+	    var d = [0,0,0,0,0,0];
+	    d.forEach(function(e,i) {
+		d[i] = 2*(X[i] - H[i]);
+	    });
+	    console.log("d",d);
+	    return [v,d];
+	}
+	
+	var p = function(X,E,Y,Z) {
+	    var v = 0;
+	    E.forEach(function(e,n) {
+		var i = e[0];
+		var j = e[1];
+		var A = new THREE.Vector2(X[i*2],X[i*2+1]);
+		var B = new THREE.Vector2(X[j*2],X[j*2+1]);
+		var len = distance(A,B);
+		if (len < Y[n]) {
+		    v += -2* (X[i] - X[j]);
+		}
+		if (len > Z[n]) {
+		    v += 2* (X[i] - X[j]);
+		}
+	    }
+	    var d = [0,0,0,0,0,0];
+	    return [v,d];	    
+	}
+
+        var solution = fmin.conjugateGradient(f, fxprime, params);
+	console.log(solution);
+    });
+    
 
 });
