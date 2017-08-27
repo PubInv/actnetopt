69d5a89c3c1179b764f122fb5579667c72c2f1f1
Robert L. Read <read.robert@gmail.com>
2017-08-27 14:15:28 -0500

    some progress in moving

--- cpp_implementation/actnetopt.cpp
+++ cpp_implementation/actnetopt.cpp
@@ -25,12 +25,25 @@ using namespace boost;
 
 // Okay, we now attempt to develope a model.
 
+// These are a number of ways of creating solvers.
+// choose a solver
+//    cppoptlib::BfgsSolver<TFindCoords> solver;
+//  cppoptlib::BfgsSolver<TFindCoords> solver;
+//  cppoptlib::ConjugatedGradientDescentSolver<T> solver;
+//  cppoptlib::NewtonDescentSolver<T> solver;
+//  cppoptlib::NelderMeadSolver<TFindCoords> solver;
+//    cppoptlib::LbfgsSolver<TFindCoords> solver;
+// cppoptlib::CMAesSolver<TFindCoords> solver;
+
+
 class ActNet {
 public:
+
+  // I know this is poor C++, I am not a very good C++ coder
   typedef adjacency_list<vecS, vecS, bidirectionalS> Graph;
   // Make convenient labels for the vertices
-  enum { A, B, C, D, E, N };
-  const int num_vertices = N;
+  enum { A, B, C, D, E, F };
+  const int num_vertices = 6;
   const char* name = "ABCDE";
 
   // writing out the edges in the graph
@@ -38,23 +51,43 @@ public:
 
   Graph g;
   Edge e;
-  Edge edge_array[7];  
+  Edge edge_array[7];
+
+  std::vector<int> fixed_nodes;
+
+  int node_fixing_order[6];
   
   typedef property_map<Graph, vertex_index_t>::type IndexMap;
   
   typedef graph_traits<Graph>::vertex_iterator vertex_iter;
-
   
   IndexMap index;
+
+  // This is 
+  Eigen::VectorXd coords[6];
   
   ActNet() {
-   // Edge ea[] = 
-   //    { Edge(A,B), Edge(A,D), Edge(C,A), Edge(D,C),
-   // 	Edge(C,E), Edge(B,D), Edge(D,E) };
-   edge_array[0] = Edge(A,B);
+
+    edge_array[0] = Edge(A,B);
+    edge_array[1] = Edge(B,C);
+    edge_array[2] = Edge(A,C);      
+    edge_array[3] = Edge(D,C);
+    edge_array[4] = Edge(C,E);
+    edge_array[5] = Edge(B,D);
+    edge_array[6] = Edge(D,E);
+
+    fixed_nodes.push_back(A);
+    fixed_nodes.push_back(B);
+
+    node_fixing_order[0] = A;
+    node_fixing_order[1] = B;
+    node_fixing_order[2] = C;
+    node_fixing_order[3] = D;
+    node_fixing_order[4] = E;
+    node_fixing_order[5] = F;
     
     const int num_edges = sizeof(edge_array)/sizeof(edge_array[0]);
-
+    
     // declare a graph object
     Graph gg(num_vertices);
     g = gg;
@@ -65,93 +98,107 @@ public:
 
     index = get(vertex_index, g);
   }
-  
 };
   
-int pretend_to_create_a_graph()
-{
-  // create a typedef for the Graph type
-  typedef adjacency_list<vecS, vecS, bidirectionalS> Graph;
-
-  // Make convenient labels for the vertices
-  enum { A, B, C, D, E, N };
-  const int num_vertices = N;
-  const char* name = "ABCDE";
-
-  // writing out the edges in the graph
-  typedef std::pair<int, int> Edge;
-  Edge edge_array[] = 
-    { Edge(A,B), Edge(A,D), Edge(C,A), Edge(D,C),
-      Edge(C,E), Edge(B,D), Edge(D,E) };
-  const int num_edges = sizeof(edge_array)/sizeof(edge_array[0]);
-
-  // declare a graph object
-  Graph g(num_vertices);
-
-  // add the edges to the graph object
-  for (int i = 0; i < num_edges; ++i)
-    add_edge(edge_array[i].first, edge_array[i].second, g);
-
-  typedef property_map<Graph, vertex_index_t>::type IndexMap;
-  IndexMap index = get(vertex_index, g);
-
-  std::cout << "vertices(g) = ";
-
-  std::pair<ActNet::vertex_iter, ActNet::vertex_iter> vp;
-  for (vp = vertices(g); vp.first != vp.second; ++vp.first)
-    std::cout << index[*vp.first] <<  " ";
-  std::cout << std::endl;
-  return 0;
-}
-
-
 // we define a new problem for optimizing the rosenbrock function
 // we use a templated-class rather than "auto"-lambda function for a clean architecture
 template<typename T>
 class FindCoords : public cppoptlib::Problem<T> {
-  public:
-    using typename cppoptlib::Problem<T>::TVector;
-    using typename cppoptlib::Problem<T>::THessian;
-
-    // this is just the objective (NOT optional)
+public:
+  using typename cppoptlib::Problem<T>::TVector;
+  using typename cppoptlib::Problem<T>::THessian;
+  
+  Eigen::VectorXd a;
+  Eigen::VectorXd b;
+  //     const double dab = 1.5; // This is in fact a constant in our frame
+  const double dac = 1.5; // these are in fact inputs to the problem
+  const double dbc = 1.5;
+  const double dcd = 1.5;
+  const double dbd = 1.5;
+  
+  // this is just the objective (NOT optional)
   // This input is an x,y position for c
   // The lengths between a, b, and c are constants (effectively, input to the problem)
-    T value(const TVector &x) {
+  T value(const TVector &x) {
+    // Eigen::VectorXd aa(2);
+    // a = aa;
+    
+    // Eigen::VectorXd bb(2);
+    // b = bb;
+    
+    Eigen::VectorXd y(2); y  << x[0], x[1];
 
-      Eigen::VectorXd a(2); a << 0, 0;
-      Eigen::VectorXd b(2); b << 0, 1.5;
-      //     const double dab = 1.5; // This is in fact a constant in our frame
-      const double dac = 1.5; // these are in fact inputs to the problem
-      const double dbc = 1.5;
-      const double dcd = 1.5;
-      const double dbd = 1.5;
-
-      Eigen::VectorXd y(2); y  << x[0], x[1];
-      Eigen::VectorXd z(2); z  << x[2], x[3];      
-
-      Eigen::VectorXd bc(2);
-      bc = y - b;
-      Eigen::VectorXd ac(2);
-      ac = y - a;
-      Eigen::VectorXd bd(2);
-      bd = b - z;
-      Eigen::VectorXd cd(2);
-      cd = y - z;
-      return
-	pow(ac.norm() - dac,2) +
-	pow(bc.norm() - dbc,2) +
-	pow(bd.norm() - dbd,2) +
-	pow(cd.norm() - dcd,2);
-    }
-
+    Eigen::VectorXd bc(2);
+    bc = y - b;
+    Eigen::VectorXd ac(2);
+    ac = y - a;
+    // We actually need to add some sort of "being on the right side test here".
+    return
+      pow(ac.norm() - dac,2) +
+      pow(bc.norm() - dbc,2);
+  }
 };
 
-int main(int argc, char const *argv[]) {
+// This is an attempt to use FindCoords in the correct order to find all of the things
+void find_all_coords(ActNet an) {
 
     // initialize the Rosenbrock-problem
     typedef FindCoords<double> TFindCoords;
     TFindCoords f;
 
+    Eigen::VectorXd temp0(2); temp0 << 0, 0;
+    an.coords[0] = temp0;
+
+    Eigen::VectorXd temp1(2); temp1 << 0, 1.5;
+    an.coords[1] = temp1;
+
+    Eigen::VectorXd a(2);
+    f.a = a;
+    f.a << 0, 0;
+    an.coords[0][0] = f.a[0];
+    an.coords[0][1] = f.a[1];    
+    
+    Eigen::VectorXd b(2);
+    f.b = b;
+    f.b << 0, 1.5;
+    an.coords[1][0] = f.b[0];
+    an.coords[1][1] = f.b[1];    
+    
+    // Basic structure: Iteratively find coordinates based on simple triangulations.
+    // This only works for actuator networks in which we can
+    int fs = an.fixed_nodes.size();
+    for(int i = fs; i < an.num_vertices; i++) {
+      // Let's set up initial values
+      // choose a starting point
+      Eigen::VectorXd x(2); x << 1, 2;
+
+      cppoptlib::LbfgsSolver<TFindCoords> solver;
+
+      f.a[0] = an.coords[i-fs][0];
+      f.a[1] = an.coords[i-fs][1];      
+      f.b[0] = an.coords[i-(fs-1)][0];
+      f.b[1] = an.coords[i-(fs-1)][1];      
+
+      // and minimize the function
+      std::cout << "f.a " << f.a << std::endl;
+      std::cout << "f.b " << f.b << std::endl;      
+      
+      solver.minimize(f, x);
+      an.coords[i][0] = x[0];
+      an.coords[i][1] = x[1];
+
+      // print argmin
+      // Now take x and make it the first coord...
+      
+      std::cout << "argmin " << x.transpose() << std::endl;
+      std::cout << "f(x) " << f(x) << std::endl;
+      
+      std::cout << "node processed" << i << std::endl;
+    }
+};
+
+
+int main(int argc, char const *argv[]) {
     ActNet an = ActNet();
     std::cout << "vertices(g) = ";
 
@@ -159,34 +206,12 @@ int main(int argc, char const *argv[]) {
     for (vp = vertices(an.g); vp.first != vp.second; ++vp.first)
       std::cout << an.index[*vp.first] <<  " ";
     std::cout << std::endl;
-    
 
-    // choose a starting point
-    Eigen::VectorXd x(4); x << 1, 2, 3, 4;
+    for (int i = 0; i < an.num_vertices; i++) {
+      Eigen::VectorXd a(2);
+      an.coords[i] = a;      
+      an.coords[i] << 0, 0;      
+    }
 
-    // first check the given derivative 
-    // there is output, if they are NOT similar to finite differences
-    //    bool probably_correct = f.checkGradient(x);
-    //    std::cout << "probably_correct: " << probably_correct << std::endl;
-    
-    // choose a solver
-    //    cppoptlib::BfgsSolver<TFindCoords> solver;
-    // cppoptlib::BfgsSolver<TFindCoords> solver;
-    //cppoptlib::ConjugatedGradientDescentSolver<T> solver;
-    //cppoptlib::NewtonDescentSolver<T> solver;
-    // cppoptlib::NelderMeadSolver<TFindCoords> solver;
-    cppoptlib::LbfgsSolver<TFindCoords> solver;
-    // cppoptlib::CMAesSolver<TFindCoords> solver;
-    // and minimize the function
-    
-    // and minimize the function
-    solver.minimize(f, x);
-    // print argmin
-    std::cout << "argmin " << x.transpose() << std::endl;
-    std::cout << "f(x) " << f(x) << std::endl;
-
-
-    //    pretend_to_create_a_graph();
-    
-    return 0;
+    find_all_coords(an);
 }
