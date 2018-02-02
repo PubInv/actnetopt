33e1d10b7ac33755e5ff15be50e4f13728f9c397
Robert L. Read <read.robert@gmail.com>
2018-02-02 11:44:29 -0600

    adding GNU license

--- dlib/src/Invert.h
+++ dlib/src/Invert.h
@@ -1,3 +1,19 @@
+// Invert.h -- Code needed to perform gradient-based inverse problem (design) of a 2D Warren Truss
+// Copyright (C) Robert L. Read, 2018
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef INVERT_H
 #define INVERT_H 1
 
